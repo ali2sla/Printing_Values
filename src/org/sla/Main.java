cd40e91ef4f8f077f5931d8efd4fe8d902771b05
alanli <alli19@scienceleadership.org>
2019-01-07 15:06:41 -0500

    Initial commit

---
+++ src/org/sla/Main.java
@@ -0,0 +1,8 @@
+package org.sla;
+
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("Computer: Counting up to ");
+            System.out.println("Computer: Counting up to ");
+        }
+}
